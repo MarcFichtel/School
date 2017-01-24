ff9b0f754332a4a726625701e3053facb28ab4a5
Marc Fichtel <MarcFichtel@users.noreply.github.com>
2017-01-24 15:17:27 -0700

    FEATURE: Make last adjustments towards program requirements
    * If no matching combination for precise change was found, search for a  combination for the highest possible change
    * Some other minor fixes
    
    TODO check Unload method

--- SENG301/A1/seng301-assignments-master/seng301-assignments-master/seng301-asgn1/seng301-asgn1/src/Frontend1/VendingMachine.cs
+++ SENG301/A1/seng301-assignments-master/seng301-assignments-master/seng301-asgn1/seng301-asgn1/src/Frontend1/VendingMachine.cs
@@ -13,11 +13,11 @@ namespace seng301_asgn1.src.Frontend1
         // Fields
         private List<int> coinKinds;                                                            // Type of coins accepted by this machine
         private int selectButtons;                                                              // Number of selection buttons on this machine
-        private Dictionary<string , int> popNamesAndCosts = new Dictionary<string, int>();            // Dictionary maps pops to costs accepted by this machine
+        private Dictionary<string , int> popNamesAndCosts = new Dictionary<string, int>();      // Dictionary maps pops to costs accepted by this machine
         private Dictionary<int, List<Coin>> loadedCoins = new Dictionary<int, List<Coin>>();    // Dictionary maps a coin kind index to the number of coins for loaded (i.e. change) coins
         private Dictionary<int, List<Pop>> loadedPops = new Dictionary<int, List<Pop>>();       // Dictionary maps a pop kind index to the number of pops for loaded pops
-        private List<Coin> insertedCoins = new List<Coin>();                                                       // A list of inserted (i.e. profit) coins
-        private List<Deliverable> deliveryChute = new List<Deliverable>();                                                // List of contents in the machine's delivery chute (change, unaccepted coins, pop) 
+        private List<Coin> insertedCoins = new List<Coin>();                                    // A list of inserted (i.e. profit) coins
+        private List<Deliverable> deliveryChute = new List<Deliverable>();                      // List of contents in the machine's delivery chute (change, unaccepted coins, pop) 
 
         // Constructor
         public VendingMachine (List<int> coinKinds, int selectButtons)
@@ -52,7 +52,7 @@ namespace seng301_asgn1.src.Frontend1
         // Load coins
         public void LoadCoins (int coinKindIndex, List<Coin> coins)
         {
-            loadedCoins.Add(coinKindIndex, coins);
+            loadedCoins.Add(coinKinds[coinKindIndex], coins);
         }
 
         // Load pops
@@ -101,19 +101,16 @@ namespace seng301_asgn1.src.Frontend1
                     int changeAvailable = 0;                                // Compute available change 
                     for (int i = 0; i < loadedCoins.Count; i++) 
                     {
-                        int numCoins = loadedCoins[i].Count;
-                        for (int j = 0; j < numCoins; j++) {
-                            changeAvailable += loadedCoins[i][j].Value;
-                        }
+                        changeAvailable += coinKinds[i] * loadedCoins[coinKinds[i]].Count;
                     }
 
                     /*
                      Check if coins can be combined to the required change
-                     --> Add max number of greatest value coins until sum is above required change
-                     --> Don't include last coin, then repeat for smaller value coins
-                     --> If combination cannot be created, repeat by starting at second highest value coin and so on
-                     --> If combination cannot be created after cycling through all values, do no dispense change
-                     --> For edge cases, repeat the above starting at the minimum
+                        --> Add max number of greatest value coins until sum is above required change
+                        --> Don't include last coin, then repeat for smaller value coins
+                        --> If combination cannot be created, repeat by starting at second highest value coin and so on
+                        --> If combination cannot be created after cycling through all values, do no dispense change
+                        --> For edge cases, repeat the above starting at the minimum
 
                      Note: This assumes that coins are ordered by value, but this is not necessary
                     */
@@ -121,75 +118,43 @@ namespace seng301_asgn1.src.Frontend1
                     int changeActual = 0;                                                       // Initialize actual change
                     if (changeAvailable >= changeRequired)                                      // Check if enough change is present
                     {
-                        // Look for matching coin combinations starting at min value
-                        for (int h = 0; h < loadedCoins.Count; h++)                             // Cycle forward through coin kinds
+                        while (changeRequired > 0)
                         {
-                            changeActual = 0;                                                   // Reset changeActual to 0
-                            List<Coin> changeCoins = new List<Coin>();                          // List of the actual change coins
-                            for (int i = h; i < loadedCoins.Count; i++)                         // Cycle forward through remaining coin kinds
-                            {
-                                for (int j = 0; j < loadedCoins[i].Count; j++)                  // Cycle through coins
-                                {
-                                    // Add coin value to changeActual if the sum doesn't exceep changeRequired
-                                    if (changeActual + loadedCoins[i][j].Value <= changeRequired) 
-                                    {
-                                        changeActual += loadedCoins[i][j].Value;
-                                        changeCoins.Add(loadedCoins[i][j]);                     // Track this specific coin
-                                    }
 
-                                    // If this combination matches changeRequired, dispense these coins
-                                    if (changeActual == changeRequired) {
-                                        
-                                        // Dispense coins
-                                        foreach (Coin c in changeCoins) {
-                                            deliveryChute.Add(c);                               // Add coin to delivery chute
+                            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                            // Forward
+                            ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-                                            for (int m = 0; m < loadedCoins.Count; m++) {       // Remove coin from loaded coins
-                                                if (loadedCoins[m].Contains(c)) {
-                                                    loadedCoins[m].Remove(c);
-                                                }
-                                            }
-                                        }
-
-                                        // Break out of third for loop
-                                        break;
-                                    }
-                                }
-                                // If a successful match was found, break out of second for loop
-                                if (changeActual == changeRequired)
-                                    break;
-                            }
-                            // If a successful match was found, break out of first for loop
-                            if (changeActual == changeRequired)
-                                break;
-                        }
-
-                        // Only do the reverse run-through if the first was unsuccessful
-                        if (changeActual != changeRequired) {
-                            // Look for matching coin combinations starting at max value
-                            for (int h = loadedCoins.Count - 1; h >= 0; h--)                        // Cycle backwards through coin kinds
+                            // Look for matching coin combinations starting at min value
+                            for (int h = 0; h < loadedCoins.Count; h++)                             // Cycle forward through coin kinds
                             {
                                 changeActual = 0;                                                   // Reset changeActual to 0
                                 List<Coin> changeCoins = new List<Coin>();                          // List of the actual change coins
-                                for (int i = h; i >= 0; i--)                                        // Cycle backwards through remaining coin kinds
+                                for (int i = h; i < loadedCoins.Count; i++)                         // Cycle forward through remaining coin kinds
                                 {
-                                    for (int j = 0; i < loadedCoins[i].Count; j++)                  // Cycle through coins
+                                    for (int j = 0; j < loadedCoins[coinKinds[i]].Count; j++)       // Cycle through coins
                                     {
-                                        // Add coin value to changeActual if the sum doesn't exceep changeRequired
-                                        if (changeActual + loadedCoins[i][j].Value <= changeRequired)
-                                            changeActual += loadedCoins[i][j].Value;
-                                            changeCoins.Add(loadedCoins[i][j]);                     // Track this specific coin
+                                        // Add coin kind value to changeActual if the sum doesn't exceep changeRequired
+                                        if (changeActual + coinKinds[i] <= changeRequired)
+                                        {
+                                            changeActual += coinKinds[i];
+                                            changeCoins.Add(loadedCoins[coinKinds[i]][j]);          // Track this specific coin
+                                        }
 
                                         // If this combination matches changeRequired, dispense these coins
-                                        if (changeActual == changeRequired) {
+                                        if (changeActual == changeRequired)
+                                        {
 
                                             // Dispense coins
-                                            foreach (Coin c in changeCoins) {
+                                            foreach (Coin c in changeCoins)
+                                            {
                                                 deliveryChute.Add(c);                               // Add coin to delivery chute
 
-                                                for (int m = 0; m < loadedCoins.Count; m++) {       // Remove coin from loaded coins
-                                                    if (loadedCoins[m].Contains(c)) {
-                                                        loadedCoins[m].Remove(c);
+                                                for (int m = 0; m < loadedCoins.Count; m++)
+                                                {
+                                                    if (loadedCoins[coinKinds[m]].Contains(c))
+                                                    {
+                                                        loadedCoins[coinKinds[m]].Remove(c);        // Remove coin from loaded coins
                                                     }
                                                 }
                                             }
@@ -206,6 +171,71 @@ namespace seng301_asgn1.src.Frontend1
                                 if (changeActual == changeRequired)
                                     break;
                             }
+
+                            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                            // Backward
+                            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+                            // Only do the reverse run-through if the first was unsuccessful
+                            if (changeActual != changeRequired)
+                            {
+                                // Look for matching coin combinations starting at max value
+                                for (int h = loadedCoins.Count - 1; h >= 0; h--)                        // Cycle backwards through coin kinds
+                                {
+                                    changeActual = 0;                                                   // Reset changeActual to 0
+                                    List<Coin> changeCoins = new List<Coin>();                          // List of the actual change coins
+                                    for (int i = h; i >= 0; i--)                                        // Cycle backwards through remaining coin kinds
+                                    {
+                                        for (int j = 0; i < loadedCoins[coinKinds[i]].Count; j++)       // Cycle through coins
+                                        {
+                                            // Add coin value to changeActual if the sum doesn't exceep changeRequired
+                                            if (changeActual + coinKinds[i] <= changeRequired)
+                                            {
+                                                changeActual += coinKinds[i];
+                                                changeCoins.Add(loadedCoins[coinKinds[i]][j]);          // Track this specific coin
+                                            }
+
+                                            // If this combination matches changeRequired, dispense these coins
+                                            if (changeActual == changeRequired)
+                                            {
+
+                                                // Dispense coins
+                                                foreach (Coin c in changeCoins)
+                                                {
+                                                    deliveryChute.Add(c);                               // Add coin to delivery chute
+
+                                                    for (int m = 0; m < loadedCoins.Count; m++)
+                                                    {
+                                                        if (loadedCoins[coinKinds[m]].Contains(c))
+                                                        {
+                                                            loadedCoins[coinKinds[m]].Remove(c);        // Remove coin from loaded coins
+                                                        }
+                                                    }
+                                                }
+
+                                                // Break out of third for loop
+                                                break;
+                                            }
+                                        }
+                                        // If a successful match was found, break out of second for loop
+                                        if (changeActual == changeRequired)
+                                            break;
+                                    }
+                                    // If a successful match was found, break out of first for loop
+                                    if (changeActual == changeRequired)
+                                        break;
+                                }
+                            }
+                            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                            // Done
+                            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+                            // If a successful match was found, break out of while loop
+                            if (changeActual == changeRequired)
+                                break;
+
+                            // Else decrement change required to look for next biggest possible combination
+                            changeRequired--;
                         }
                     }
                 }
