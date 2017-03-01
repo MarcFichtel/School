ab347150e915a4e235fa9b6a40870dfb1baa10bd
Marc Fichtel <MarcFichtel@users.noreply.github.com>
2017-03-01 12:54:08 -0700

    ADD initGPIO and wait functions

--- CPSC359/A3/source.s
+++ CPSC359/A3/source.s
@@ -6,7 +6,7 @@
 // * Cardin Chen, 10161477
 //
 // TODOs
-// * Everything
+// * 
 //
 // Program does the following:
 // * Take input from a SNES controller
@@ -38,9 +38,10 @@ main:
 
 	// Print names
 	LDR 	r0, =authors 						// Load 'author' string into register
-	BL 	stringBitCount 						// Branch to count bits & print
+	BL 	Print_Message 						// Branch to count bits & print
   
-  
+  	// Initialize GPIO
+	BL 	Init_GPIO 						// Set up GPIO
   
   
   
@@ -72,66 +73,101 @@ loopCounter:
 //////////////////////////////////////////////////////
 
 Init_GPIO:
-  PUSH 	{lr}							        // Start function
+  	PUSH 	{lr}						        // Start function
   
-  // Code here
+  	// Init CLOCK (Set GPIO11 to output)
+	LDR 	r0, =0x3F000004 					// Address for GPFSEL1
+	LDR 	r1, [r0]						// Copy GPFSEL1 into r1
+	MOV 	r2, #7 							// (r2 = 0b111)
+	LSL 	r2, #3 							// Index of 1st bit for pin11, r2 = 0 111 000
+	BIC 	r1, r2 							// Clear pin11 bits
+	MOV 	r3 , #1 						// Output function code
+	LSL 	r3, #3 							// Shift 1 (output) over for pin11
+	ORR 	r1, r3 							// Set pin11 function in r1 to 1 (output)
+	STR 	r1, [r0] 						// Write back to GPFSEL1
   
-  POP 	{pc} 							        // End function
+  	// Init LATCH (Set GPIO9 to output)
+	LDR 	r0, =0x3F000000 					// Address for GPFSEL0
+	LDR 	r1, [r0]						// Copy GPFSEL0 into r1
+	MOV 	r2, #7 							// (r2 = 0b111)
+	LSL 	r2, #27 						// Index of 1st bit for pin9
+	BIC 	r1, r2 							// Clear pin9 bits
+	MOV 	r3 , #1 						// Output function code
+	LSL 	r3, #27 						// Shift 1 (output) over for pin9
+	ORR 	r1, r3 							// Set pin9 function in r1 to 1 (output)
+	STR 	r1, [r0] 						// Write back to GPFSEL0
+	
+	// Init DATA (Set GPIO10 to input)
+  	LDR 	r0, =0x3F000004 					// Address for GPFSEL1
+	LDR 	r1, [r0]						// Copy GPFSEL1 into r1
+	MOV 	r2, #7 							// (r2 = 0b111)
+	BIC 	r1, r2 							// Clear pin10 bits
+	ORR 	r1, r3 							// Set pin10 function in r1 to 0 (input)
+	STR 	r1, [r0] 						// Write back to GPFSEL1
+  
+  
+  	POP 	{pc} 						        // End function
   
 //////////////////////////////////////////////////////
 // Write to Latch
 //////////////////////////////////////////////////////
   
 Write_Latch:
-  PUSH 	{lr}							        // Start function
+  	PUSH 	{lr}						        // Start function
   
-  // Code here
+  	// Code here
   
-  POP 	{pc} 							        // End function
+  	POP 	{pc} 						        // End function
   
 //////////////////////////////////////////////////////
 // Write to Clock
 //////////////////////////////////////////////////////
   
 Write_Clock:
-  PUSH 	{lr}							        // Start function
+  	PUSH 	{lr}						        // Start function
   
-  // Code here
+  	// Code here
   
-  POP 	{pc} 							        // End function
+  	POP 	{pc} 						        // End function
   
 //////////////////////////////////////////////////////
 // Read Data
 //////////////////////////////////////////////////////
   
 Read_Data:
-  PUSH 	{lr}							        // Start function
-  
-  // Code here
-  
-  POP 	{pc} 							        // End function
+	  PUSH 	{lr}							// Start function
+
+	  // Code here
+
+	  POP 	{pc} 							// End function
  
 //////////////////////////////////////////////////////
 // Wait
 //////////////////////////////////////////////////////
  
 Wait:
-  PUSH 	{lr}							        // Start function
+  	PUSH 	{lr}							// Start function
   
-  // Code here
+	LDR 	r0, =0x3F003004 					// Address of CLO
+	LDR 	r1, [r0] 						// Read CLO
+	ADD 	r1, #12 						// Add 12 micros
+waitLoop:
+	LDR 	r2, [r0] 						// Load CLO
+	CMP 	r1, r2 							// If CLO < r1
+	Bhi 	waitLoop 						// Go to waitLoop
   
-  POP 	{pc} 							        // End function
+  	POP 	{pc} 							// End function
   
 //////////////////////////////////////////////////////
 // Read SNES
 //////////////////////////////////////////////////////
   
 Read_SNES:
-  PUSH 	{lr}							        // Start function
+	PUSH 	{lr}							// Start function
   
-  // Code here
+  	// Code here
   
-  POP 	{pc} 							        // End function
+  	POP 	{pc} 							// End function
   
 //////////////////////////////////////////////////////
 // Data Section
@@ -140,15 +176,15 @@ Read_SNES:
 .section 	.data
 .align
 
-authors: 	.asciz 	"Created by Marc-Andre Fichtel and Cardin Chen\n\r"
+authors: 		.asciz 	"Created by Marc-Andre Fichtel and Cardin Chen\n\r"
 
 button_press: 		.asciz 	"Please press a button...\n\r"
-pressed_A: 			.asciz 	"You have pressed A \n\r"
-pressed_B: 			.asciz 	"You have pressed B \n\r"
-pressed_X: 			.asciz 	"You have pressed X \n\r"
-pressed_Y: 			.asciz 	"You have pressed Y \n\r"
-pressed_L: 			.asciz 	"You have pressed LEFT BUMPER \n\r"
-pressed_R: 			.asciz 	"You have pressed RIGHT BUMPER \n\r"
+pressed_A: 		.asciz 	"You have pressed A \n\r"
+pressed_B: 		.asciz 	"You have pressed B \n\r"
+pressed_X: 		.asciz 	"You have pressed X \n\r"
+pressed_Y: 		.asciz 	"You have pressed Y \n\r"
+pressed_L: 		.asciz 	"You have pressed LEFT BUMPER \n\r"
+pressed_R: 		.asciz 	"You have pressed RIGHT BUMPER \n\r"
 pressed_joyRIGHT: 	.asciz 	"You have pressed Joy-pad RIGHT \n\r"
 pressed_joyLEFT: 	.asciz 	"You have pressed Joy-pad LEFT \n\r"
 pressed_joyUP: 		.asciz 	"You have pressed Joy-pad UP \n\r"
@@ -156,4 +192,4 @@ pressed_joyDOWN: 	.asciz 	"You have pressed Joy-pad DOWN \n\r"
 pressed_START: 		.asciz 	"You have pressed START \n\r"
 pressed_SELECT: 	.asciz 	"You have pressed SELECT \n\r"
 
-end: 		.asciz "Program is terminating...\n\r"
+end: 			.asciz "Program is terminating...\n\r"
