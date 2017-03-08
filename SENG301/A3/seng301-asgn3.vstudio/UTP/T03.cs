add25db89748761432e70e268ccf9ca1081954c9
Marc Fichtel <MarcFichtel@users.noreply.github.com>
2017-03-08 11:14:24 -0700

    ADD third test T03

--- SENG301/A3/seng301-asgn3.vstudio/UTP/T03.cs
+++ SENG301/A3/seng301-asgn3.vstudio/UTP/T03.cs
@@ -1,5 +1,8 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Frontend2.Hardware;
+using Frontend2;
 
 namespace UTP {
 
@@ -17,6 +20,72 @@ namespace UTP {
 
         [TestMethod]
         public void Test03() {
+
+            // CREATE(5, 10, 25, 100; 3; 10; 10; 10)
+            int[] coinKinds = { 5, 10, 25, 100 };
+            int buttonCount = 3;
+            int coinRackCap = 10;
+            int popsRackCap = 10;
+            int receptacCap = 10;
+            VendingMachine vm = new VendingMachine(coinKinds, buttonCount, coinRackCap, popsRackCap, receptacCap);
+
+            // Initialize vending machine logic object
+            VendingMachineLogic vml = new VendingMachineLogic(vm);
+
+            // EXTRACT([0])
+            IDeliverable[] contentsList = vm.DeliveryChute.RemoveItems();   // Remove items from delivery chute
+            string[] contents = new string[2];                              // List tracks dispensed pop and change
+            int coinsValue = 0;                                             // Variable to hold value of change
+            for (int i = 0; i < contentsList.Length; i++) {                 // Iterate over dispensed items
+                if (contentsList[i].GetType() == typeof(Coin)) {            // if dispensed item is a coin...
+                    Coin c = (Coin)contentsList[i];                         // Cast it as a coin, then...
+                    coinsValue += c.Value;                                  // Add its value to coinsValue
+                } else {                                                    // Else the dispensed item is a pop, so... 
+                    contents[i] = contentsList[i].ToString();               // Add each pop's name to contents
+                }
+                if (coinsValue > 0) {                                       // If change was dispensed,...
+                    contents[1] = coinsValue.ToString();                    // Add its value to contents
+                }
+            }
+
+            // CHECK_DELIVERY(0)
+            // TODO Check if its possible to assert two lists or arrays are the same
+            string[] expected = {null, null };           // Set up expected result
+            for (int i = 0; i < contents.Length; i++) {     // Iterate over contents
+                Assert.AreEqual(contents[i], expected[i]);  // Assert each content element is as expected
+            }
+
+            // UNLOAD([0])
+            int storedCoinsValue = 0;                   // Variable for value of all stored coins
+            List<Coin> storedCoins = new List<Coin>();  // variable for tracking a set of coins
+
+            foreach (CoinRack cr in vm.CoinRacks) {     // Iterate over coin racks
+                storedCoins = cr.Unload();              // Unload coin rack
+                foreach (Coin c in storedCoins) {       // Iterate over coins in coin rack
+                    storedCoinsValue += c.Value;        // Add each coin's value to value of all stored coins
+                }
+            }
+
+            storedCoins = vm.StorageBin.Unload();       // Unload storage bin
+            foreach (Coin c in storedCoins) {           // Iterate over coins in storage bin
+                storedCoinsValue += c.Value;            // Add each coin's value to value of all stored coins
+            }
+
+            List<string> pops = new List<string>();     // Variable for tracking stored pop names
+            List<PopCan> temp = new List<PopCan>();     // Temporary variable for stored pops
+            foreach (PopCanRack pcr in vm.PopCanRacks) {// Iterate over pop can racks
+                temp.AddRange(pcr.Unload());            // Add each rack's contents to temp
+            }
+            foreach (PopCan pc in temp) {               // Iterate over all pops in temp
+                pops.Add(pc.Name);                      // Add each pop's name to pops
+            }
+
+            // CHECK_TEARDOWN(0; 0)
+            // TODO Doublecheck what the second number represents
+            int expected1 = 0;                                  // Variable holds expected result 1
+            Assert.AreEqual(storedCoinsValue, expected1);       // Assert that stored coins value is as expected
+            Assert.AreEqual(pops.Count, expected1);             // Assert each unloaded pop is as expected
+
         }
     }
 }
