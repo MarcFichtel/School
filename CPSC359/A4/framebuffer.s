2ffb657c36a23d9a0148a5a2ed5559f14e906ad2
Marc Fichtel <MarcFichtel@users.noreply.github.com>
2017-03-27 12:56:03 -0600

    ADD drawing routines

--- CPSC359/A4/framebuffer.s
+++ CPSC359/A4/framebuffer.s
@@ -1,14 +1,14 @@
 .section .text
 
-.global InitFrameBuffer
-
 //////////////////////////////////////////////////////////////////////////////////////
 // Initialize the FrameBuffer using the FrameBufferInit structure
 // Code taken from tut08 example
 // Returns:
 // ~ r0: 0 on failure, framebuffer pointer on success
 //////////////////////////////////////////////////////////////////////////////////////
- 
+
+.globl InitFrameBuffer 			  // Make function global
+
 InitFrameBuffer:
 	LDR	r2, =0x3F00B880           // Get address of mailbox0
 	LDR	r3, =FrameBufferInit      // Get address of framebuffer init structure
@@ -48,6 +48,308 @@ pointerWaitLoop$:
 	LDR 	r3, =FrameBufferPointer
 	STR	r0, [r3]
 	BX	lr
+	
+//////////////////////////////////////////////////////////////////////
+// Clear Screen (paints display black)
+/////////////////////////////////////////////////////////////////////
+
+.globl ClearScreen		  // Make function global
+
+ClearScreen:
+
+	PUSH 	{r4-r6, lr}
+
+	MOV 	r4, #0 		// X = 0
+	MOV 	r5, #0 		// Y = 0
+	MOV 	r6, #0 		// Color = Black
+
+loop:
+	MOV 	r0, r4
+	MOV 	r1, r5
+	MOV 	r2, r6
+	BL 	DrawPixel 	// DrawPixel (X, Y, #white)
+
+	ADD 	r4, #1 		// X++
+	CMP 	r4, #1024 	// If X < 1024, loop, else...
+	Blt 	loop
+
+	MOV 	r4, #0 		// X = 0
+	ADD 	r5, #1 		// Y++
+	CMP 	r5, #768 	// If Y < 768, loop, else...
+	Blt 	loop
+
+	POP 	{r4-r6, pc} 	// End function
+
+//////////////////////////////////////////////////////////////////////
+// Draw a Square
+// Inputs:
+// ~ r0: Starting point X
+// ~ r1: Starting point Y
+// ~ r2: Square Size
+// ~ r3: Color
+/////////////////////////////////////////////////////////////////////
+
+.globl DrawSquare		  // Make function global
+
+DrawSquare:
+
+	PUSH 	{r4-r9, lr}
+
+	pointX 	.req r4
+	pointY 	.req r5
+	color 	.req r6
+	size 	.req r7
+	borderX .req r8
+	borderY .req r9
+
+	MOV 	pointX, r0 		// X = r0
+	MOV 	pointY, r1 		// Y = r1
+	MOV 	color, r3 		// Color = r3
+	MOV 	size, r2 		// Size = r2
+	
+	MOV 	borderX, size
+	ADD 	borderX, pointX
+	MOV 	borderY, size
+	ADD 	borderY, pointY
+
+loop0:
+	MOV 	r0, pointX
+	MOV 	r1, pointY
+	MOV 	r2, color
+	BL 		DrawPixel 		// DrawPixel(X, Y, color)
+
+	ADD 	pointX, #1 		// X++
+	CMP 	pointX, borderX // If X < size, loop, else...
+	Blt 	loop0
+
+	SUB 	pointX, size 	// X -= 40
+	ADD 	pointY, #1 		// Y++
+	CMP 	pointY, borderY // If Y < border, loop, else...
+	Blt 	loop0
+
+	.unreq pointX
+	.unreq pointY
+	.unreq color
+	.unreq size
+	.unreq borderX
+	.unreq borderY
+
+	POP 	{r4-r9, pc} 	// End function
+
+//////////////////////////////////////////////////////////////////////
+// Draw Pixel
+// ~ r0: X-coordinate
+// ~ r1: Y-coordinate
+// ~ r2: Color
+/////////////////////////////////////////////////////////////////////
+
+.globl DrawPixel 		  		// Make function global
+
+DrawPixel:
+	PUSH	{r4, lr} 			// Start function
+	ADD	r4, r0, r1, lsl #10 	// r4 = (y * 1024) + x = x + (y << 10)
+	LSL	r4, #1 					// r4 *= 2 (for 16 bits per pixel = 2 bytes per pixel)
+	LDR	r0, =FrameBufferPointer	// Get frame buffer pointer address
+	LDR	r0, [r0] 				// Load frame buffer pointer into r0
+	STRH	r2, [r0, r4] 		// Store color (hword) at framebuffer pointer + offset
+	POP	{r4, pc}				// End function
+
+//////////////////////////////////////////////////////////////////////
+// Draw a Character (in white)
+// ~ r0: X-coordinate
+// ~ r1: Y-coordinate
+// ~ r2: Character
+/////////////////////////////////////////////////////////////////////
+
+.globl DrawChar		  // Make function global
+
+DrawChar:
+	PUSH	{r4-r10, lr}
+
+	charAddr .req	r4
+	pixel_x	 .req	r5
+	pixel_y	 .req	r6
+	row	 .req	r7
+	mask	 .req	r8
+	char	 .req 	r9
+	temp 	 .req 	r10
+
+	LDR	charAddr, =font		// Load address of font map
+	MOV	pixel_x, r0		// Move x position to safe reg
+	MOV 	temp, r0 		// Move x position to temporary reg
+	MOV	pixel_y, r1		// Move y position to safe reg
+	MOV 	char, r2 		// Move char to safe reg
+	ADD	charAddr, char, LSL #4	// Char address = font base + (char * 16)
+
+charLoop$:
+	MOV	pixel_x, temp		// Restore X to initial value
+	MOV	mask,	#0x01		// Set bitmask to 1 in the LSB
+	LDRB	row, [charAddr], #1	// Load row byte, post-increment charAddr
+
+rowLoop$:
+	TST	row, mask		// Test row byte against bitmask
+	Beq	noPixel$ 		// If equal, go to noPixel$, else...
+
+	MOV	r0, pixel_x		// 1st arg: X coord
+	MOV	r1, pixel_y 		// 2st arg: Y coord
+	MOV	r2, #0xFFFFFFFF		// 3rd arg: Color (white)
+	BL	DrawPixel		// DrawPixel(X, Y, white)
+
+noPixel$:
+	ADD	pixel_x, #1		// Increment X coord
+	LSL	mask, #1		// Shift bitmask left by 1
+
+	TST	mask, #0x100		// Test if bitmask has shifted 8 times (i.e. test 9th bit)
+	Beq	rowLoop$ 		// If it has shifted 8 times, loop to rowLoop, else...
+	ADD	pixel_y, #1		// Increment Y coordinate
+
+	TST	charAddr, #0xF 		// Test if address is evenly divisible by 16
+	Bne	charLoop$ 		// If so, loop to charLoop$ (ie: the next char)
+
+	.unreq	charAddr
+	.unreq	pixel_x
+	.unreq	pixel_y
+	.unreq	row
+	.unreq	mask
+	.unreq 	char
+	.unreq 	temp
+
+	POP	{r4-r10, pc} 		// Else, end function
+
+//////////////////////////////////////////////////////////////////////
+// Draw a String (in white)
+// ~ r0: X-coordinate
+// ~ r1: Y-coordinate
+// ~ r2: String Address
+/////////////////////////////////////////////////////////////////////
+
+.globl DrawString		  // Make function global
+
+DrawString:
+	PUSH	{r5-r7, lr} 		// Start function
+
+	pointX	.req r5 		// Name registers
+	pointY	.req r6
+	strAddr .req r7
+
+	MOV 	pointX, r0 		// Move input to safe registers
+	MOV 	pointY, r1
+	MOV 	strAddr, r2
+
+loop1:
+	LDRB 	r2, [strAddr], #1 	// Load byte into r3
+	CMP 	r2, #0 			// Compare current byte to null
+	
+	Beq 	done1 			// If equal, string is done
+	BL 	DrawChar		// Else, DrawChar(X, Y, current_character)
+	ADD 	pointX, #20		// Offset next character
+	MOV 	r0, pointX
+	MOV 	r1, pointY 		
+	B 	loop1 			// Then loop to next character
+	
+done1:
+	.unreq 	pointX 			// Unname registers
+	.unreq 	pointY
+	.unreq 	strAddr
+
+	POP	{r5-r7, pc} 		// End function
+
+//////////////////////////////////////////////////////////////////////
+// Draw an Image (each image is 40x40)
+// ~ r0: X-coordinate
+// ~ r1: Y-coordinate
+// ~ r2: Image Address
+/////////////////////////////////////////////////////////////////////
+
+.globl DrawImage		  // Make function global
+
+DrawImage:
+	PUSH 	{r4-r9, lr}
+
+	pointX 	.req r4
+	pointY 	.req r5
+	img 	.req r6
+	size 	.req r7
+	borderX .req r8
+	borderY .req r9
+
+	MOV 	pointX, r0 		// X = r0
+	MOV 	pointY, r1 		// Y = r1
+	MOV 	img, r2 		// img = r3
+	MOV 	size, #40 		// All images are 40x40
+	
+	MOV 	borderX, size
+	ADD 	borderX, pointX
+	MOV 	borderY, size
+	ADD 	borderY, pointY
+
+loop2:
+	MOV 	r0, pointX
+	MOV 	r1, pointY
+	LDRH 	r2, [img], #2
+	BL 		DrawPixel 		// DrawPixel(X, Y, color)
+
+	ADD 	pointX, #1 		// X++
+	CMP 	pointX, borderX // If X < size, loop, else...
+	Blt 	loop2
+
+	SUB 	pointX, size 	// X -= 40
+	ADD 	pointY, #1 		// Y++
+	CMP 	pointY, borderY // If Y < border, loop, else...
+	Blt 	loop2
+
+	.unreq pointX
+	.unreq pointY
+	.unreq img
+	.unreq size
+	.unreq borderX
+	.unreq borderY
+
+	POP 	{r4-r9, pc} 	// End function
+
+//////////////////////////////////////////////////////////////////////
+// Draw the BG
+/////////////////////////////////////////////////////////////////////
+
+.globl DrawBG
+
+DrawBG:
+	PUSH 	{r4-r8, lr}
+
+	pointX 	.req r4
+	pointY 	.req r5
+	img 	.req r6
+	borderX .req r7
+	borderY .req r8
+
+	MOV 	pointX, #0 		// X = r0
+	MOV 	pointY, #0 		// Y = r1
+	LDR 	img, =startmenu // img = r3
+	MOV 	borderX, #640
+	MOV 	borderY, #480
+
+loop3:
+	MOV 	r0, pointX
+	MOV 	r1, pointY
+	LDRH 	r2, [img], #2
+	BL 		DrawPixel 		// DrawPixel(X, Y, color)
+
+	ADD 	pointX, #1 		// X++
+	CMP 	pointX, borderX // If X < size, loop, else...
+	Blt 	loop3
+
+	SUB 	pointX, #640 	// X -= 40
+	ADD 	pointY, #1 		// Y++
+	CMP 	pointY, borderY // If Y < border, loop, else...
+	Blt 	loop3
+
+	.unreq pointX
+	.unreq pointY
+	.unreq img
+	.unreq borderX
+	.unreq borderY
+
+	POP 	{r4-r8, pc} 	// End function
 
 //////////////////////////////////////////////////////////////////////////////////////
 // Data Section
