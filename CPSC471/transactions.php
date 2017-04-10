df66a76d08cfa13df6b34675b9d88323c4e918b7
nikolaiaguirre <nikolai.aguirre@ucalgary.ca>
2017-04-09 18:46:39 -0600

    Added features
    - Should update the a_id of transactions with the admin_id of the first product of the transaction
    - Should increase the stock of the products in the refunded transaction by 1
    - Should add back the sum of prices in the transaction to funds of customer

--- CPSC471/transactions.php
+++ CPSC471/transactions.php
@@ -34,22 +34,56 @@
                 <?php
                 //checks if the form from the select buttons are pressed
                 if (!empty($_POST['t_id'])){
+                    
                     //fetches the product ids from the transaction id
                     $t_products = mysqli_query($conn, ""
                             . "SELECT TransactionProducts.product_id FROM Transaction, TransactionProducts"
                             . "WHERE TransactionProducs.transaction_id = '".$_POST['t_id']."' ");
+                    
                     //takes the first row of that table since we're setting the admin id as the first admin id in the list of products
                     $p_row = mysqli_fetch_assoc($t_products);
+                    
                     //finds the product with the same product_id as the first product in the transaction and finds the admin_id from it
                     $admin_id = mysqli_query($conn, ""
-                            . "SELECT Product.admin_id FROM Product "
+                            . "SELECT Product.admin_id, Product.price FROM Product "
                             . "WHERE Product.id = '".$p_row["product_id"]."'");
                     $a_row = mysqli_fetch_assoc($admin_id);
+                    
                     //updates the transaction admin_id to the one obtained from first product in transaction
+                    //not sure if we can update the admin_id directly
                     $insert_id = mysqli_query($conn, ""
                             . "UPDATE Transaction"
                             . "SET Transaction.admin_id = '".$a_row["admin_id"]."' "
                             . "WHERE Transaction.id = '".$_POST['t_id']."'");
+                    
+                    //updates the stock of the product
+                    //don't know if I can do the + 1 to Product.stock in the sqli query
+                    $update_stock = mysqli_query($conn, ""
+                            . "UPDATE Product"
+                            . "SET Product.stock = Product.stock+1"
+                            . "WHERE Product.id = '".$p_row["product_id"]."'");
+                    while($stock = mysqli_fetch_assoc($p_row) > 0){
+                        $update_stock = mysqli_query($conn, ""
+                            . "UPDATE Product"
+                            . "SET Product.stock = Product.stock+1"
+                            . "WHERE Product.id = '".$p_row["product_id"]."'");
+                    }
+                    
+                    //add the funds back to the customers wallet
+                    //gets a table full of prices of products that were in the transactions
+                    $product_list = mysqli_query($conn, ""
+                            . "SELECT Product.price FROM Product, TransactionProducts"
+                            . "WHERE TransactionProducts.transaction_id = '".$_POST['t_id']."'"
+                            . "  AND TransactionProducts.product_id = Product.id");
+                    //adds the prices in the list with the matching list id and product id
+                    while($prices = mysqli_fetch_assoc($product_list) > 0){
+                        $update_product = mysqli_query($conn, ""
+                                . "UPDATE Customer"
+                                . "SET Customer.funds = Customer.funds+'".$prices['price']."'"
+                                . "WHERE Customer.username = '".$_SESSION['Username']."'" );
+                        
+                    }
+                    
                 }
                 //creates a table that shows each transaction
                 if (mysqli_num_rows($retrieveId) > 0){
