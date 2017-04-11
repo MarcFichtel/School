6b667cd5833e69d915ebdee756b8eef092dbd9b6
marc.a.fichtel@gmail.com <mf@shore.com>
2017-04-11 10:53:14 -0600

    Fix transaction table styling

--- CPSC471/transactions.php
+++ CPSC471/transactions.php
@@ -1,4 +1,7 @@
-<?php include "base.php"; ?>
+<?php 
+include "base.php"; 
+session_start();
+?>
 
 
 <!DOCTYPE html>
@@ -20,89 +23,146 @@
 			
                 <!--Navgation-->
                 <div id="navi">
-                    <!--UserPage-->
-                    <a href="demoPage.php">Main Page</a>
-                </div>
-                <?php
-                //retrieves the transaction id from the customer id
-                    $sql = "SELECT transaction.id, transaction.date FROM transaction, customer WHERE customer.username = '".$_SESSION['Username']."' AND customer.id = transaction.customer_id";
-                    $retrieveId = mysqli_query($conn, $sql);
-                ?>
-                
-                <?php
-                //checks if the form from the select buttons are pressed
-                if (!empty($_POST['t_id'])){
-                    //fetches the product ids from the transaction id
-                    $t_products = mysqli_query($conn, ""
-                            . "SELECT Transactionproducts.product_id FROM Transactionproducts "
-                            . "WHERE Transactionproducts.transaction_id = ".$_POST['t_id']." ");
                     
-                    //takes the first row of that table since we're setting the admin id as the first admin id in the list of products
+                    <!--User Login-->
+                    <a id="userHomeButton" href="demoPage.php">User Home</a>
+
+                    <!--Employee Login-->
+                    <a id="employeeHomeButton" href="employeeLogin.php">Employee Home</a>
+
+                    <!--Admin Login-->
+                    <a id="adminHomeButton" href="adminLogin.php">Admin Home</a>
+                    
+                </div>
+            <?php
+                // Retrieves the transaction id from the customer id
+                $username = $_SESSION['Username'];
+                $sql = "SELECT Transaction.id, Transaction.date "
+                        . "FROM Transaction, Customer "
+                        . "WHERE Customer.username = '".$username."' "
+                        . "AND Customer.id = Transaction.customer_id";
+                $retrieveId = mysqli_query($conn, $sql);
+                if (!$retrieveId) {
+                    echo "<h1>Error</h1>";
+                    echo "Failed to retrieve transaction ids.";
+                    echo "<code>", mysqli_error($conn), "</code>";
+                } else {
+                    echo "<h1>Your Transactions</h1>";
+                }
+
+                // Checks if the form from the select buttons are pressed
+                if (!empty($_POST['t_id'])) {
+                    
+                    // Fetches the product ids from the transaction id
+                    $t_id = $_POST['t_id'];
+                    $t_products = mysqli_query($conn, ""
+                            . "SELECT TransactionProducts.product_id FROM TransactionProducts "
+                            . "WHERE TransactionProducts.transaction_id = '".$t_id."' ");
+                    if (!$t_products) {
+                        echo "<h1>Error</h1>";
+                        echo "Failed to retrieve product ids.";
+                        echo "<code>", mysqli_error($conn), "</code>";
+                    }
+                    
+                    // Takes the first row of that table since we're setting 
+                    // the admin id as the first admin id in the list of products
                     $p_row = mysqli_fetch_assoc($t_products);
                     
-                    //finds the product with the same product_id as the first product in the transaction and finds the admin_id from it
-                    $sql = "SELECT admin_id, price FROM product WHERE id = ".$p_row['product_id']."";
+                    // Finds the product with the same product_id as the first 
+                    // product in the transaction and finds the admin_id from it
+                    $pId = $p_row['product_id'];
+                    $sql = "SELECT admin_id, price FROM Product WHERE id = '".$pId."'";
                     $admin_id = mysqli_query($conn, $sql);
+                    if (!$admin_id) {
+                        echo "<h1>Error</h1>";
+                        echo "Failed to retrieve admin ids.";
+                        echo "<code>", mysqli_error($conn), "</code>";
+                    }
                     $a_row = mysqli_fetch_assoc($admin_id);
 
-                    //updates the transaction admin_id to the one obtained from first product in transaction
-                    //not sure if we can update the admin_id directly because it's a foreign key
-
-                    $sql = "UPDATE Transaction SET admin_id = ".$a_row['admin_id']." WHERE id = ".$_POST['t_id']."";
+                    // Updates the transaction admin_id to the one obtained from 
+                    // first product in transaction
+                    // TODO Not sure if we can update the admin_id directly 
+                    // because it's a foreign key
+                    $aId = $a_row['admin_id'];
+                    $sql = "UPDATE Transaction SET admin_id = '".$aId."' WHERE id = '".$t_id."' ";
                     $insert_id = mysqli_query($conn, $sql);
+                    if (!$insert_id) {
+                        echo "<h1>Error</h1>";
+                        echo "Failed to update transaction's admin ids.";
+                        echo "<code>", mysqli_error($conn), "</code>";
+                    }
                     
-                    //updates the stock of the product
-                    //don't know if I can do the + 1 to Product.stock in the sqli query
-                    $sql = "UPDATE Product SET stock = stock+1 WHERE id = ".$p_row['product_id']."";
-                    $update_stock = mysqli_query($conn, $sql);
-                    
+                    // Updates the stock of the product
+                    // TODO Don't know if I can do the + 1 to Product.stock 
+                    // in the sqli query
                     while($stock = mysqli_fetch_assoc($t_products)){
-                        $sql = "UPDATE Product SET stock = stock+1 WHERE id = ".$stock['product_id']."";
+                        $pId = $stock['product_id'];
+                        $sql = "UPDATE Product SET stock = stock+1 WHERE id = '".$pId."' ";
                         $update_stock = mysqli_query($conn, $sql);
+                        if (!$update_stock) {
+                            echo "<h1>Error</h1>";
+                            echo "Failed to update product stock.";
+                            echo "<code>", mysqli_error($conn), "</code>";
+                        }
                     }
                     
-                    //add the funds back to the customers wallet
-                    //gets a table full of prices of products that were in the transactions
-                    $sql = "SELECT product.price FROM product, transactionproducts WHERE transactionproducts.transaction_id = ".$_POST['t_id']." AND transactionproducts.product_id = product.id";
+                    // Add the funds back to the customers wallet
+                    // Gets a table full of prices of products that were in the transactions
+                    $sql = "SELECT Product.price FROM Product, TransactionProducts "
+                            . "WHERE TransactionProducts.transaction_id = '".$t_id."' "
+                            . "AND TransactionProducts.product_id = Product.id";
                     $product_list = mysqli_query($conn, $sql);
-
-                    //adds the prices in the list with the matching list id and product id
-                    while($prices = mysqli_fetch_assoc($product_list)){
-                        $sql = "UPDATE Customer SET funds = funds+".$prices['price']." WHERE username = '".$_SESSION['Username']."'";
+                    if (!$update_stock) {
+                        echo "<h1>Error</h1>";
+                        echo "Failed to retrieve product prices.";
+                        echo "<code>", mysqli_error($conn), "</code>";
+                    }
+                        
+                    // Adds the prices in the list with the matching list id and product id
+                    while ($prices = mysqli_fetch_assoc($product_list)) {
+                        $price = $prices['price'];
+                        $sql = "UPDATE Customer "
+                                . "SET funds = funds+'".$price."' "
+                                . "WHERE username = '".$username."'";
                         $update_product = mysqli_query($conn, $sql);
+                        if (!$update_stock) {
+                            echo "<h1>Error</h1>";
+                            echo "Failed to add up prices.";
+                            echo "<code>", mysqli_error($conn), "</code>";
+                        }
                     }
-                    
                 }
-                //creates a table that shows each transaction
-                if (mysqli_num_rows($retrieveId) > 0){
-                    echo "<table border='2' class='stats' cellspacing='2'>
-                        <tr>
-                        <td class='hed' colspan='8'>Transactions</td>
-                        </tr>
-                        <tr>
-                        <th>Id</th>
-                        <th>Date</th>
-                        <th>Refund</th>
-                        </tr>";
+                
+                // Creates a table that shows each transaction
+                if (mysqli_num_rows($retrieveId) == 0){
+                    echo "There are no transactions at the moment. "
+                        . "Once you make a purchase, the transaction will appear here.";
+                }
+                echo "<table id='transactiontable'>";
+                    echo "<tr>";
+                        echo "<th>ID</th>";
+                        echo "<th>Date</th>";
+                        echo "<th>Refund</th>";
+                    echo "</tr>";
 
-                    while ($row = mysqli_fetch_assoc($retrieveId)){
-                        echo "<tr>";
-                        echo "<td>".$row["id"];
-                        echo "<td>".$row["date"];
-                        echo "<td>"; 
-            ?>
-                        <form method="POST" action="transactions.php" name = "form">
-                        <fieldset>
-                            <input value="<?php echo $row["id"];?>" type="hidden" name="t_id">
-                            <input type="submit"  value="Select">
-                        </fieldset>
-                        </form>
-            <?php
-                        echo "</td></tr>";
-                    }
+                while ($row = mysqli_fetch_assoc($retrieveId)){
+                    $id = $row['id'];
+                    $date = $row['date'];
+                    echo "<tr>";
+                        echo "<td>", $id, "</td>";
+                        echo "<td>", $date, "</td>";
+                        echo "<td>''</td>"; 
+
+                    echo "<form method='POST' action='transactions.php' name='transactionsform'>";
+                        echo "<fieldset>";
+                            echo "<input value='".$id."' type='hidden' name='t_id'>";
+                            echo "<input type='submit'  value='Select'>";
+                        echo "</fieldset>";
+                    echo "</form>";
                 }
             ?>  
                 
-            </div>
-	</body>
+        </div>
+    </body>
 </html>
