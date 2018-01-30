80184abac802d6ecd64a4e8e7d43ac1f64af6b0b
marc.a.fichtel@gmail.com <marc.a.fichtel@gmail.com>
2018-01-30 15:45:06 -0700

    Implemented first couple of functions

--- SENG513/A2/code.js
+++ SENG513/A2/code.js
@@ -2,16 +2,94 @@
 // this is just a stub for a function you need to implement
 //
 function getStats(txt) {
-    return {
-        nChars: 123,
-        nWords: 22,
-        nLines: 10,
-        nNonEmptyLines: 22,
-        averageWordLength: 3.3,
-        maxLineLength: 33,
-        palindromes: ["12321", "kayak", "mom"],
-        longestWords: ["xxxxxxxxx", "123444444"],
-        mostFrequentWords: ["hello(7)", "world(1)"]
-    };
+
+  let result = {
+    nChars: countChars(txt),
+    nWords: countWords(txt),
+    nLines: countLines(txt),
+    nNonEmptyLines: countNonEmptyLines(txt),
+    maxLineLength: findMaxLineLength(txt),
+    averageWordLength: findAvgWordLength(txt),
+    palindromes: findPalindromes(txt),
+    longestWords: findLongestWords(txt),
+    mostFrequentWords: findMostFrequentWords(txt)
+  }
+
+    return result;
 }
 
+//
+// Count characters in a string
+//
+function countChars(txt) {
+  return txt.length;
+}
+
+//
+// Count words in a string
+//
+function countWords(txt) {
+  // Convert newlines to spaces and trim the string
+  txt = txt.replace(/\n/g, " ").trim();
+  // Split at spaces
+  let words = txt.split(" ");
+
+  // TODO solve example 3
+
+  return words.length;
+}
+
+//
+// Count lines in a string
+//
+function countLines(txt) {
+  return txt.split("\n").length;
+}
+
+//
+// TODO
+//
+function countNonEmptyLines(txt) {
+  let countNL = 0;
+  for (let i = 0; i < txt.length; i++) {
+    if (txt[i] === "\n") {
+      countNL++;
+    }
+  }
+  return countNL;
+}
+
+//
+// Find the length of the longest line in a string
+//
+function findMaxLineLength(txt) {
+  return null;
+}
+
+//
+// Find the average word length in a string
+//
+function findAvgWordLength(txt) {
+  return null;
+}
+
+//
+// Find all palindromes in a string
+//
+function findPalindromes(txt) {
+  return null;
+}
+
+//
+// Find the longest words in a string
+//
+function findLongestWords(txt) {
+  return null;
+}
+
+//
+// Find the most frequent words in a string
+//
+function findMostFrequentWords(txt) {
+  return null;
+}
